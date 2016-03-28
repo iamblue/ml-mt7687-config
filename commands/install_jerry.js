687a08cfc95a4c185511df3cf467026acb43f21a
iamblue <tonyone0902@gmail.com>
2016-03-28 16:41:05 +0800

    change __dirname to path.join method

--- commands/install_jerry.js
+++ commands/install_jerry.js
@@ -1,4 +1,5 @@
 var child = require('child_process');
+var path = require('path');
 module.exports = function(arg, generate, done) {
-  child.exec('cp -R ' + __dirname.replace('/commands', '/node_modules/ml-core-jerryscript/cm4/jerryscript') + ' ' + process.env.PWD + '/sdk/middleware/jerryscript && cp ' + __dirname.replace('/commands', '/templates/jerry-port.c') + ' ' + process.env.PWD + '/sdk/project/mt7687_hdk/apps/iot_sdk/src/jerry-port.c' );
+  child.exec('cp -R ' + path.join(__dirname, '../node_modules/ml-core-jerryscript/cm4/jerryscript') + ' ' + process.env.PWD + '/sdk/middleware/jerryscript && cp ' + path.join(__dirname, '../templates/jerry-port.c') + ' ' + process.env.PWD + '/sdk/project/mt7687_hdk/apps/iot_sdk/src/jerry-port.c' );
 }
--- commands/install_ml.js
+++ commands/install_ml.js
@@ -1,4 +1,5 @@
 var child = require('child_process');
+var path  = require('path');
 module.exports = function(arg, generate, done) {
-  child.exec('cp -R ' + __dirname.replace('/commands', '/templates/microlattice.h') + ' ' + process.env.PWD + '/sdk/project/mt7687_hdk/apps/iot_sdk/inc/microlattice.h');
+  child.exec('cp -R ' + path.join(__dirname, '../templates/microlattice.h') + ' ' + process.env.PWD + '/sdk/project/mt7687_hdk/apps/iot_sdk/inc/microlattice.h');
 }
