c537c1648730d246ff0bb838dfb201e9b3c4010c
iamblue <tonyone0902@gmail.com>
2016-03-28 16:38:41 +0800

    change __dirname to path.join method

--- commands/init_mt7687.js
+++ commands/init_mt7687.js
@@ -1,7 +1,7 @@
 var child = require('child_process');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require("fs"));
-
+var path = require('path');
 module.exports = function(arg, generate, done) {
   var package = require(process.env.PWD + '/package.json');
   if (!package.hasOwnProperty('dependencies')) {
@@ -14,5 +14,5 @@ module.exports = function(arg, generate, done) {
   package.scripts.build = "ml parse:js && ml build:js && ml build:init && ml build:bin && ml burn ./out/mt7687_iot_sdk_xip.bin";
   package.scripts.installEnv = "ml install:sdk && ml install:gcc && ml install:jerry && ml install:ml";
   fs.writeFileAsync(process.env.PWD + '/package.json', JSON.stringify(package));
-  return child.exec('cp ' + __dirname.replace('/commands', '/featureConfig.json') + ' ' + process.env.PWD + '/featureConfig.json && mkdir sdk');
+  return child.exec('cp ' + path.join(__dirname, '../featureConfig.json') + ' ' + process.env.PWD + '/featureConfig.json && mkdir sdk');
 }
