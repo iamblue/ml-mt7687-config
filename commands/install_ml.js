0c80ad2eaf4178dbf3014078f55272b510ddf9d7
iamblue <tonyone0902@gmail.com>
2016-05-02 17:58:26 +0800

    adjust permission

--- commands/install_ml.js
+++ commands/install_ml.js
@@ -1,6 +1,6 @@
 var child = require('child_process');
 var path  = require('path');
 module.exports = function(arg, generate, done) {
-  child.exec('cp -R ' + path.join(__dirname, '../templates/build.sh') + ' ' + process.env.PWD + '/build.sh');
+  child.exec('cp -R ' + path.join(__dirname, '../templates/build.sh') + ' ' + process.env.PWD + '/build.sh && chmod 777 ./build.sh');
   child.exec('cp -R ' + path.join(__dirname, '../templates/microlattice.h') + ' ' + process.env.PWD + '/sdk/project/mt7687_hdk/apps/iot_sdk/inc/microlattice.h');
 }
