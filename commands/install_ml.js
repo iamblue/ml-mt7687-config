d30be9470f4f5dc478b705224f42e8a0dbffe108
iamblue <tonyone0902@gmail.com>
2016-05-02 20:32:28 +0800

    add windows.sh

--- commands/install_ml.js
+++ commands/install_ml.js
@@ -1,6 +1,9 @@
 var child = require('child_process');
 var path  = require('path');
 module.exports = function(arg, generate, done) {
+  if (process.platform === 'win32') {
+    child.exec('cp -R ' + path.join(__dirname, '../templates/windows.sh') + ' ' + process.env.PWD + '/windows.sh && chmod 777 ./windows.sh');
+  }
   child.exec('cp -R ' + path.join(__dirname, '../templates/build.sh') + ' ' + process.env.PWD + '/build.sh && chmod 777 ./build.sh');
   child.exec('cp -R ' + path.join(__dirname, '../templates/microlattice.h') + ' ' + process.env.PWD + '/sdk/project/mt7687_hdk/apps/iot_sdk/inc/microlattice.h');
 }
---
+++ templates/windows.sh
@@ -0,0 +1,9 @@
+ml install:jerry && ml install:ml && ml install:resetSDK &&
+
+cd sdk/project/mt7687_hdk/apps/iot_sdk/src/ &&
+
+mkdir ml &&
+
+cd ml && mkdir src &&
+
+cd ../../../../../../
