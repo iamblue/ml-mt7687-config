922eab2498d23b7caa0a45a52bbbe49e78189247
po-ju.chen <po-ju.chen@mediatek.com>
2016-08-12 11:49:48 +0800

    add general style

--- commands/burn_xmodem.js
+++ commands/burn_xmodem.js
@@ -7,6 +7,7 @@ module.exports = function(arg, generate, done) {
   var sdkPath = process.env.PWD + '/sdk';
 
   var burn = child.exec('python ' + uploaderUrl + '/upload.py -c ' + featureConfig.download_port + ' -f ./out/mt7687_hdk/iot_sdk_demo/mt7687_iot_sdk_demo.bin', { cwd: sdkPath });
+
   burn.stdout.on('data', function(data) {
     console.log(data);
   });
@@ -15,7 +16,7 @@ module.exports = function(arg, generate, done) {
   });
   burn.on('exit', function() {
     console.log('==============================================================');
-    console.log('Success! Download! ');
+    console.log('Success!'.green + ' Download! ');
     console.log('==============================================================');
   });
 }
