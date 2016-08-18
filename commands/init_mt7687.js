086bcac16668fe6443afad579df3ed2c0ed5e3a5
po-ju.chen <tonyone0902@gmail.com>
2016-08-18 11:12:28 +0800

    fixed some bugs for windows

--- commands/init_mt7687.js
+++ commands/init_mt7687.js
@@ -14,8 +14,8 @@ module.exports = function(arg, generate, done) {
   // package.dependencies['ml-mcs'] = "*";
   package.SDKpath = "./sdk";
   package.SDKversion = "3.3.1";
-  package.scripts.build = "./build.sh";
-  package.scripts.installEnv = "ml install:sdk && ml install:gcc && ml install:jerry && ml install:ml && ml install:resetSDK && ml install:cache";
+  package.scripts.build = "sh ./build.sh";
+  package.scripts.installEnv = "cd sdk && tar -xvf ./LinkIt_SDK_V3.3.1_public.tar.gz && cd .. && ml install:gcc && ml install:jerry && ml install:ml && ml install:resetSDK && ml install:cache";
   fs.writeFileAsync(process.env.PWD + '/package.json', JSON.stringify(package, null, '\t'));
   return child.exec('cp ' + path.join(__dirname, '../featureConfig.json') + ' ' + process.env.PWD + '/featureConfig.json && mkdir sdk');
 }
